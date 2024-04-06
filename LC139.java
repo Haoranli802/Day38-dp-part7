54c704cea25acc6f3b54286f71f89284975f9452
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-04-06 12:09:08 +0800

    Update LC139.java

--- LC139.java
+++ LC139.java
@@ -24,3 +24,33 @@ class Solution {
         return dp[s.length()];
     }
 }
+
+
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        /**
+        dp[j]: j为长度的字符是否全部都由字典里面的字组成
+        初始：dp[0] 为true其他都是false；
+        公式: dp[j] && set.contains(s.substring(j, i))
+        遍历：先物品再背包
+
+        O(N * M), O(N)
+         */
+        //Set<String> set = new HashSet<>();
+        //for(String str: wordDict) set.add(str);
+        boolean[] dp = new boolean[s.length() + 1];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+        for(int i = 1; i <= s.length(); i++){
+            for(String str : wordDict){
+                int len = str.length();
+                if(i >= len && dp[i - len] && str.equals(s.substring(i - len, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
